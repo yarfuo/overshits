ba96ffa6044b231d80f24bcc8f83676ba1a196f5
Yarfuo <yarfuo@gmail.com>
2021-08-06 12:19:56 +0300

    feat: search results cleaning functionality

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Google search cleaner for shitty stackoverflow clones
 # Why?
 There is already working solutions like [this](https://github.com/iorate/uBlacklist#uBlacklist) and there is not only one.
 I just wanted to create "install and forgot" self updating extension specifically for stackovershits.
+
+# TODO
+- Fix page jumping on removing results
--- src/block.ts
+++ src/block.ts
@@ -1,6 +1,25 @@
 import { browser } from "webextension-polyfill-ts"
 
 (async () => {
-  const blocklist = await browser.runtime.sendMessage({type: "retrieveBlocklist"})
-  console.log(blocklist)
+  const blocklistUrls: string[] = await browser.runtime.sendMessage({type: "retrieveBlocklist"})
+  console.log(blocklistUrls)
+  const searchResults = document.querySelectorAll(".g")
+  searchResults.forEach(searchResult => {
+    const h3 = searchResult.querySelector("a > h3")
+    if (!h3) {
+      return
+    }
+    const linkElement = h3.parentElement
+    if (!linkElement) {
+      return
+    }
+    const link = linkElement.getAttribute("href")
+    if (!link) {
+      return
+    }
+    const host = (new URL(link)).host
+    if (blocklistUrls.includes(host)) {
+      searchResult.remove()
+    }
+  })
 })()
